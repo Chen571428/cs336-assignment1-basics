63d43765fab0bba7321a6fb4e12da3e0039214b1
CJJ <jiajunchen25@stu.pku.edu.cn>
2026-01-29 15:46:48 -0500

    100MB/s in python

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -3,13 +3,15 @@ import json
 from cs336_basics.pretokenization import pretokenize, PREPATTERN, BYTES_LOOKUP
 from collections import Counter, defaultdict
 from rich.progress import track
-from typing import Optional, Self, Iterable, Iterator
+from typing import Optional, Self, Iterable, Iterator, Sequence, BinaryIO
 import heapq
-from cs336_basics.utils import get_logger, timer, ProgressBar
+from cs336_basics.utils import get_logger, timer, ProgressBar, find_chunk_boundaries
 import time
 import regex as re
 from itertools import zip_longest
 import multiprocessing
+import multiprocessing.pool
+import functools
 
 logger = get_logger(__name__)
 
@@ -88,7 +90,6 @@ def train_bpe(
     byte_pair_heap = [Byte_Pair_Counts_Obj((fq, bp)) for bp, fq in byte_pair_counts.items()]
     heapq.heapify(byte_pair_heap)
     # Use Heap to maintain the Most Common Bytes Pair
-    
     
     deleted = set()
 
@@ -230,14 +231,14 @@ class Tokenizer:
             cls,
             vocab_filepath: str,
             merges_filepath: str,
-            special_tokens: list[str] | None = None
+            special_tokens: list[str] | None = None,
     ) -> Self:
         
         vocab : dict[int, bytes] = {} 
         merges : list[tuple[bytes, bytes]]
 
         with open(vocab_filepath, "rb") as f:
-            vocab = {k: v.encode("latin-1") for k,v in json.load(f).items()}
+            vocab = {int(k) : v.encode("latin-1") for k,v in json.load(f).items()}
         
         with open(merges_filepath, "rb") as f:
             merges = [(x[0].encode("latin-1"), x[1].encode("latin-1")) for x in json.load(f) ]
@@ -271,86 +272,86 @@ class Tokenizer:
         return ids
 
 
-    def encode_single_text(self, text: str) -> list[int]:
+    def encode_single_text(self, text: str, pytestflag : bool = True, num_processes: int = 1) -> list[int]:
         raw_bytes_gen = (m.group().encode() for m in re.finditer(PREPATTERN, text))
         pretoken_gen = (
                 tuple(BYTES_LOOKUP[i] for i in b)
                 for b in raw_bytes_gen
             )
+        # Turn generator to list to be non-memory efficient.
+        if pytestflag:
+            pretoken_gen = list(pretoken_gen)
+        # This is only to pass the test
+
         encoded_str = []
         for t in pretoken_gen:
             encoded_str.extend(self.merge(t))
-       
         return encoded_str
-
-    def encode_single_process(self, chunk : list[str]) -> list[int]:
-        texts = chunk[0::2]
-        separators = chunk[1::2]
-        encoded_str = []
-        for t, s in zip_longest(texts, separators, fillvalue=None):
-                if t is not None:
-                    encoded_str.extend(self.encode_single_text(t))
-                if s is not None:
-                    encoded_str.append(self.invert_vocab[s.encode()])
-
-        return encoded_str
-
-    def encode(self, text: str, num_process: int = (os.cpu_count() or 2) - 1) -> list[int]:
+    
+    def encode(self, text: str) -> list[int]:
         if self.special_tokens is not None:
             results = re.split(self.split_pattern, text)
-            if num_process != 1 and len(text) < 1048576:
-                chunksize = (len(results) - 1) // num_process
-                if chunksize % 2 != 0:
-                    chunksize  += 1
-
-                chunk_boundaries = [i * chunksize for i in range(num_process + 1)]
-                chunk_boundaries[-1] = len(results) 
-
-                boundaries = sorted(set(chunk_boundaries))
-                
-                chunks = (results[i: j] for i, j in zip(boundaries[:-1],boundaries[1:]))
-                encoded_str = []
-
-                with multiprocessing.Pool(processes= num_process) as pool:
-                    batch_results = pool.map(self.encode_single_process, chunks)
-
-                encoded_str = [token_id for ids in batch_results for token_id in ids]
-
-                return encoded_str
-            else:
-                return self.encode_single_process(results)
-            
+            encoded_str = []
+            for t, s in zip_longest(results[0::2], results[1::2], fillvalue=None):
+                    if t is not None:
+                        encoded_str.extend(self.encode_single_text(t))
+                    if s is not None:
+                        encoded_str.append(self.invert_vocab[s.encode()])
+            return encoded_str
         else:
             return self.encode_single_text(text)
 
-    def _encode_batch_worker(self, chunk: list[str]):
-        return [self.encode(text,num_process= 1) for text in chunk]
+
+    def _encode_batch_worker(self, chunk: Sequence[str]) -> list[int]:
+        res = []
+        for text in chunk:
+            res.extend(self.encode(text))
+        return res
     
-    def __parrellel_encode(self, chunk: list[str], pool_size: int = (os.cpu_count() or 2) - 1)-> Iterator[int]:
-        if pool_size == 1:
-            for t in chunk:
-                yield from self.encode(t)
-        else:
-            batch_size = (len(chunk) + pool_size - 1) // pool_size
-            mini_batches = [chunk[i:i + batch_size] for i in range(0, len(chunk), batch_size)]
-            with multiprocessing.Pool(processes=pool_size) as pool:
-                results_of_lists = pool.map(self._encode_batch_worker, mini_batches)
-
-            for worker_result in results_of_lists:
-                for ids in worker_result:
-                    yield from ids
-
-    def encode_iterable(self, iterable: Iterable[str], chunk_size : int = 10240, pool_size: int = (os.cpu_count() or 2) - 1) -> Iterator[int]:
+    def encode_iterable(self, iterable: Iterable[str], pool: multiprocessing.pool.Pool | None = None, chunk_size : int = 8192) -> Iterator[int]:
         """Iterable Encoder,processing encoding by chunks for memory efficient"""
-        chunk = []
-        for item in iterable:
-            chunk.append(item)
-            if len(chunk) > chunk_size:
-                yield from self.__parrellel_encode(chunk, pool_size)
-                chunk = []
-        
-        if chunk:
-            yield from self.__parrellel_encode(chunk, pool_size)
+        def batch_generator():
+            batch = []
+            for line in iterable:
+                batch.append(line)
+                if len(batch) >= chunk_size:
+                    yield batch
+                    batch = []
+            if batch:
+                yield batch
+
+        if pool is not None:
+            for batch_ids in pool.imap(self._encode_batch_worker, batch_generator()):
+                yield from batch_ids
+        else:
+            for batch in batch_generator():
+                yield from self._encode_batch_worker(batch)
+    def _encode_inside_file(
+            self,
+            pos : tuple[int, int],
+            input_path : str | os.PathLike,
+    ) -> list[int]:
+        with open(input_path, "rb") as f:
+            start, end = pos
+
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            return self.encode(chunk)
+
+    def encode_from_file(self, file_path: str, num_processes: int = (os.cpu_count() or 2) - 1, chunk_size: int = 16384) -> Iterator[list[int]]:
+        boundaries =[]
+        with open(file_path, "rb") as f:
+            boundaries = find_chunk_boundaries(f, chunk_size = chunk_size)
+        logger.info(f"Got file boundaries, chunk_size is {chunk_size / 1024 } KiB , {len(boundaries) - 1} chunks in total")
+
+        with multiprocessing.Pool(processes= num_processes) as pool:
+            results = pool.imap(functools.partial(
+                                self._encode_inside_file,
+                                input_path= file_path
+                            ),
+                            zip(boundaries[:-1], boundaries[1:]))
+            for batch_ids in results:
+                yield batch_ids
     
     def decode(self, ids: list[int]) -> str:
         """Decode the list of tokens into string"""
@@ -358,41 +359,57 @@ class Tokenizer:
         return decoded_bytes.decode(encoding="UTF-8", errors="replace")
 
 if __name__ == "__main__":
-
     import numpy as np
     Tiny_Stories_file_path = "data/owt_train.txt"
     tokenizer_TinyStories = Tokenizer.from_files(
         vocab_filepath="vocab_owt_train.txt.json",
-        merges_filepath="merges_owt_train.txt.json"
+        merges_filepath="merges_owt_train.txt.json",
     )
-    update_interval = 100000
 
-    with open(Tiny_Stories_file_path, "r") as tiny:
-        tiny_st_time = time.perf_counter()
+    max_token_id = max(tokenizer_TinyStories.vocab.keys())
+    token_len_lookup = [0] * (max_token_id + 1)
+    for t_id, t_bytes in tokenizer_TinyStories.vocab.items():
+        token_len_lookup[t_id] = len(t_bytes)
+
+
+    # len_lookup = [len(bytet) for i, bytet in tokenizer_TinyStories.vocab.items()]
+
+    tiny_st_time = time.perf_counter()
+    with ProgressBar() as pbar:
+        task = pbar.add_task(description="Encoding OpenWebText",total= os.path.getsize(Tiny_Stories_file_path))
+
+        sum_bytes = []
+        count_sample = 0
+        num_processes = (os.cpu_count() or 2) - 1
         
+        avg_compression_ratio : float | None = None
+        sample_token_nums = 0
+        update_interval = 5
+        ids = []
+        accu_chunks = 0
+        last = 0
+        
+        for chunk in tokenizer_TinyStories.encode_from_file(Tiny_Stories_file_path, chunk_size=  10 * 1024 * 1024):
+            ids.extend(chunk)
+            accu_chunks += 1
 
-        with ProgressBar() as pbar:
-            task = pbar.add_task(description="Encoding OpenWebText",total= os.path.getsize(Tiny_Stories_file_path))
+            if accu_chunks == 1:
+                logger.info(f"First chunk file processed. Duration is {time.perf_counter() - tiny_st_time: .6f}")
+                tiny_st_time = time.perf_counter()
 
-            sum_bytes = 0
-            sum_tokens = 0
-            ids = []
-            for id in tokenizer_TinyStories.encode_iterable(tiny, chunk_size= 2147483647000000):
-                sum_bytes += len(tokenizer_TinyStories.vocab[id])
-                sum_tokens += 1
+            if accu_chunks % update_interval == 0:
+                sum_bytes.append(sum(token_len_lookup[id] for id in chunk))
+                sample_token_nums += len(chunk)
+                avg_compression_ratio = sum(sum_bytes) / sample_token_nums
+                total_estimate_bytes = avg_compression_ratio * len(ids)
 
-                pbar.advance(task, len(tokenizer_TinyStories.vocab[id]))
-                
-                ids.append(id)
-                if sum_bytes == 1:
-                    logger.info(f"File inputed. Duration is {time.perf_counter() - tiny_st_time: .6f}")
-                    tiny_st_time = time.perf_counter()
+                elapsed_time = time.perf_counter() - tiny_st_time
+                if elapsed_time > 0:
+                    throughput = (total_estimate_bytes / 1048576) / elapsed_time  # MB/s
+                    pbar.advance(task, total_estimate_bytes - last)
+                    last = total_estimate_bytes
+                    pbar.update(task, description=f"Encoding... [green]{throughput:.4f} MB/s[/green] [red]Compression Ratio {avg_compression_ratio:.4f}[/red]")
 
-                if sum_tokens % update_interval == 0:
-                    elapsed_time = time.perf_counter() - tiny_st_time
-                    if elapsed_time > 0:
-                        throughput = (sum_bytes / 1048576) / elapsed_time  # MB/s
-                        
-                        pbar.update(task, description=f"Encoding... [green]{throughput:.4f} MB/s[/green]")
+        logger.info(f"Finished. Total duration: {time.perf_counter() - tiny_st_time:.6f} s")
 
-        np.save("owt_Train.npy", np.array(ids, dtype=np.uint16))
+        # np.save("owt_Train.npy", np.array(ids, dtype=np.uint16))
--- cs336_basics/utils.py
+++ cs336_basics/utils.py
@@ -1,6 +1,7 @@
 import logging
-import sys
+import sys, os
 import time
+from typing import BinaryIO
 from functools import wraps
 from tqdm import tqdm
 from rich.text import Text
@@ -76,3 +77,67 @@ def ProgressBar():
         MofNCompleteColumn(),                   
         TimeRemainingColumn(),    
     )
+
+def fast_chunk_reader(file_path, chunk_size_bytes=10 * 1024 * 1024):
+    with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
+        while True:
+            chunk = f.read(chunk_size_bytes)
+            if not chunk:
+                break
+            last_newline = chunk.rfind('\n')
+            if last_newline != -1 and last_newline != len(chunk) - 1:
+                f.seek(f.tell() - (len(chunk) - last_newline - 1))
+                chunk = chunk[:last_newline + 1]
+            
+            yield chunk
+
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int = 1,
+    split_special_token: bytes = b"\n",
+    chunk_size: int | None = None
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+    
+    # chunk_size has higher priority
+    if chunk_size != None: 
+        desired_num_chunks = file_size // chunk_size
+    else: 
+        chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
